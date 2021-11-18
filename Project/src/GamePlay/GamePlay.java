3e2404fc4626f820a742937c44f81a5265b5d0db
Chengzuo Song <86466959+SongDingQing@users.noreply.github.com>
2021-11-18 00:24:24 -0500

    0.0

--- Project/src/GamePlay/GamePlay.java
+++ Project/src/GamePlay/GamePlay.java
@@ -2,8 +2,15 @@ package GamePlay;
 
 import javax.swing.*;
 
-public class GamePlay extends JFrame{
- public GamePlay(){
-
- }
+/*** 这个类绘制一个条形图
+ * @author Chengzuo Song
+ * @version 0.0
+ */
+public class GamePlay extends JFrame {
+    public GamePlay() {
+        super("budge");
+     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     setSize(1600,1200);
+     setVisible(true);
+    }
 }
