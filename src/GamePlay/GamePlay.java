2fab9ef4cc85a3dc72817fa3ddde92bd7c235e31
Chengzuo Song <86466959+SongDingQing@users.noreply.github.com>
2021-11-18 01:20:46 -0500

    0.0
    GUI basic Panels and method configured

--- src/GamePlay/GamePlay.java
+++ src/GamePlay/GamePlay.java
@@ -3,26 +3,38 @@ package GamePlay;
 import javax.swing.*;
 import java.awt.*;
 
-/*** 这个类绘制一个条形图
+/*** This Class is the the Game Drawing
  * @author Chengzuo Song
  * @version 0.0
  */
 
 
 public class GamePlay extends JFrame {
-    //CONSTANTS
+    //-----------CONSTANTS--------------
     private final int HEIGHT= 800;
     private final int WIDTH = 1200;
+    // 1. MAP CONSTANT
     private final int MAP_HEIGHT= 800;
     private final int MAP_WIDTH = 700;
+
+    // 2. CONTROL CONSTANT
     private final int CPANEL_HEIGHT= 800;
     private final int CPANEL_WIDTH = 500;
+    private final int STATUS_HEIGHT= 200;
+    private final int STATUS_WIDTH = 500;
+    private final int COMMAND_HEIGHT= 400;
+    private final int COMMAND_WIDTH = 500;
+    private final int LOG_HEIGHT= 200;
+    private final int LOG_WIDTH = 500;
 
+    //Panels Components
     private JPanel mapPanel;
     private JPanel controlPanel;
 
     public GamePlay() {
         super("budge");
+        //In Game Constant
+        int hp=1000;
         //Map Panel
         setMapPanel();
         //Control Panel
@@ -41,16 +53,43 @@ public class GamePlay extends JFrame {
     }
     private void setMapPanel(){
         mapPanel = new JPanel();
-        mapPanel.setSize(MAP_WIDTH,MAP_HEIGHT);
+        Dimension d = new Dimension(MAP_WIDTH,MAP_HEIGHT);
+        mapPanel.setPreferredSize(d);
         mapPanel.setBackground(new Color(250,200,200));
     };
     private void setControlPanel(){
         controlPanel= new JPanel();
-        controlPanel.setSize(CPANEL_WIDTH,CPANEL_WIDTH);
-        controlPanel.setBackground(new Color(200,200,250));
+        Dimension d = new Dimension(CPANEL_WIDTH,CPANEL_HEIGHT);
+        controlPanel.setPreferredSize(d);
+        controlPanel.add(createStatusPanel(),BorderLayout.NORTH);
+        controlPanel.add(createCommandPanel(),BorderLayout.CENTER);
+        controlPanel.add(createLogPanel(),BorderLayout.SOUTH);
+        controlPanel.setBackground(new Color(220,200,50));
     };
+    private JPanel createStatusPanel(){
+        JPanel statusPanel = new JPanel();
+        Dimension d = new Dimension(STATUS_WIDTH,STATUS_HEIGHT);
+        statusPanel.setPreferredSize(d);
+        statusPanel.setBackground(new Color(100,100,250));
+        return statusPanel;
+    };
+    private JPanel createCommandPanel(){
+        JPanel commandPanel = new JPanel();
+        Dimension d = new Dimension(COMMAND_WIDTH,COMMAND_HEIGHT);
+        commandPanel.setPreferredSize(d);
+        commandPanel.setBackground(new Color(250,100,100));
+        return commandPanel;
+    };
+    private JPanel createLogPanel(){
+        JPanel logPanel = new JPanel();
+        Dimension d = new Dimension(LOG_WIDTH,LOG_HEIGHT);
+        logPanel.setPreferredSize(d);
+        logPanel.setBackground(new Color(100,250,100));
+        return logPanel;
+    };
+
     private void setupPanels(){
-        add(mapPanel);
-        add(controlPanel);
+        add(mapPanel,BorderLayout.WEST);
+        add(controlPanel,BorderLayout.EAST);
     }
 }
