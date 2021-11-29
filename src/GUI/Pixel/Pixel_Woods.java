a09b962ebe616007b9b9f03024d253152196b80c
Chengzuo Song <86466959+SongDingQing@users.noreply.github.com>
2021-11-28 23:11:15 -0500

    0.0

--- src/GUI/Pixel/Pixel_Woods.java
+++ src/GUI/Pixel/Pixel_Woods.java
@@ -7,7 +7,7 @@ public class Pixel_Woods extends Pixel{
     public void drawPixel(Graphics g,int x, int y) {
         g.setColor(new Color(0, 140 , 0));
         g.fillRect(x*10, y*10+80, 10, 10);
-        g.setColor(new Color(0, 70 , 0));
+        g.setColor(new Color(0, 100 , 0));
         g.drawRect(x*10, y*10+80, 10, 10);
         g.setColor(new Color(50,50,50));
     }
