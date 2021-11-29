b939eaaae9d85942c54b9a129cfbb58c01728f45
Chengzuo Song <86466959+SongDingQing@users.noreply.github.com>
2021-11-28 23:23:40 -0500

    interface collectable added

---
+++ src/GUI/Pixel/Collectable.java
@@ -0,0 +1,6 @@
+package GUI.Pixel;
+
+public interface Collectable {
+    public int getAmount();
+    public void takeAmount(int n);
+}
--- src/GUI/Pixel/Pixel_Woods.java
+++ src/GUI/Pixel/Pixel_Woods.java
@@ -2,7 +2,14 @@ package GUI.Pixel;
 
 import java.awt.*;
 
-public class Pixel_Woods extends Pixel{
+public class Pixel_Woods extends Pixel implements Collectable{
+    private int WoodStorage;
+
+    public Pixel_Woods(){
+        super();
+        WoodStorage=1000;
+    }
+
     @Override
     public void drawPixel(Graphics g,int x, int y) {
         g.setColor(new Color(0, 140 , 0));
@@ -10,5 +17,15 @@ public class Pixel_Woods extends Pixel{
         g.setColor(new Color(0, 100 , 0));
         g.drawRect(x*10, y*10+80, 10, 10);
         g.setColor(new Color(50,50,50));
+    }
+
+    @Override
+    public int getAmount() {
+        return WoodStorage;
+    }
+
+    @Override
+    public void takeAmount(int n) {
+        WoodStorage-=n;
     }
 }
