ccf99cd8c59e1631205179ce59ac9a376a4e6631
Chengzuo Song <86466959+SongDingQing@users.noreply.github.com>
2021-12-02 18:26:20 -0500

    0.1.3
    Run added

---
+++ src/Run.java
@@ -0,0 +1,11 @@
+import NetWork.Client;
+import NetWork.Server;
+
+public class Run {
+    public static void main(String[] args){
+        Client.main(args);
+        //Client.main(args);
+        Server.main(args);
+
+    }
+}
