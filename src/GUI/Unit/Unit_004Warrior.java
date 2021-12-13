e520e4845a3d238f9b619fa36726520bb1b948db
Chengzuo Song <86466959+SongDingQing@users.noreply.github.com>
2021-12-13 11:19:25 -0500

    1.0.2
    UI update: warrior

--- src/GUI/Unit/Unit_004Warrior.java
+++ src/GUI/Unit/Unit_004Warrior.java
@@ -1,12 +1,12 @@
 package GUI.Unit;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class Unit_004Warrior extends Unit{
-
+    Image pic=new ImageIcon("src/Images/warrior.png").getImage();
     @Override
     public void drawUnit(Graphics2D g, int x, int y) {
-        g.setColor(new Color(255, 0, 255));
-        g.fillRect(x+1, y+1, 8, 8);
+        g.drawImage(pic,x+1,y+1,null);
     }
 }
