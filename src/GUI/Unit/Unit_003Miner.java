a45f629b924f34931c269569d4a8d1a7b98373ce
Chengzuo Song <86466959+SongDingQing@users.noreply.github.com>
2021-12-13 20:26:23 -0500

    1.03

--- src/GUI/Unit/Unit_003Miner.java
+++ src/GUI/Unit/Unit_003Miner.java
@@ -1,12 +1,12 @@
 package GUI.Unit;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class Unit_003Miner extends Unit{
-
+    Image pic=new ImageIcon("src/Images/miner.png").getImage();
     @Override
     public void drawUnit(Graphics2D g, int x, int y) {
-        g.setColor(new Color(150, 100, 0));
-        g.fillOval(x+1, y+1, 8, 8);
+        g.drawImage(pic,x+1, y+1, null);
     }
 }
