88630e53bbe6d3e1f32e169a9f52c9e1815ce95e
Chengzuo Song <86466959+SongDingQing@users.noreply.github.com>
2021-12-13 23:12:03 -0500

    1.1.1
    compelete version

--- src/GUI/Unit/Unit_006Fence.java
+++ src/GUI/Unit/Unit_006Fence.java
@@ -7,6 +7,6 @@ public class Unit_006Fence extends Unit{
     Image pic=new ImageIcon("src/Images/fence.png").getImage();
     @Override
     public void drawUnit(Graphics2D g, int x, int y) {
-        g.drawImage(pic,x+1,y+1,null);
+        g.drawImage(pic,x,y,null);
     }
 }
